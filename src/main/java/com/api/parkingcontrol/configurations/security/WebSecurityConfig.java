b1af460411434dc07abf7bd66b913d42b5e78eb0
Daiana Michels <daianaem.12@gmail.com>
2022-08-27 11:24:17 -0300

    feat: adiciona configuração inicial de usuário e senha

--- src/main/java/com/api/parkingcontrol/configurations/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingcontrol/configurations/security/WebSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.api.parkingcontrol.configurations.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,6 +17,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .authorizeHttpRequests()
-                .anyRequest().permitAll();
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
     }
+
+    //método para substituir as configurações padrão do usuário do spring security:
+    @Override
+    protected  void configure(AuthenticationManagerBuilder auth){
+        auth.inMemoryAuthentication()
+                .withUser("Admin")
+                .password(passwordEncoder().encode("270822"))
+                .roles("ADMIN");
+    }
+
+    //configuração do password encoder
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
