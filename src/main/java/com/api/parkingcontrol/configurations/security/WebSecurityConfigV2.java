c909b1e4d9ab8a91d735940afcca62effc191e3a
Daiana Michels <daianaem.12@gmail.com>
2022-08-27 14:51:50 -0300

    feat: configura acesso as rotas diretamente no controller

--- src/main/java/com/api/parkingcontrol/configurations/security/WebSecurityConfigV2.java
+++ src/main/java/com/api/parkingcontrol/configurations/security/WebSecurityConfigV2.java
@@ -3,15 +3,18 @@ package com.api.parkingcontrol.configurations.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+//essa anotação ativa a configuração das rotas autorizadas diretamente nos endpoints
 public class WebSecurityConfigV2 {
 
-    //nova forma de configurar o spring security, utilizando a componentização
+    //<<<<<<nova forma de configurar o spring security, utilizando a componentização>>>>
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -19,9 +22,9 @@ public class WebSecurityConfigV2 {
                 .httpBasic()
                 .and()
                 .authorizeHttpRequests()
-                .antMatchers(HttpMethod.GET, "/parking-spot/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/parking-spot").hasRole("USER")
-                .antMatchers(HttpMethod.DELETE, "/parking-spot/**").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.GET, "/parking-spot/**").permitAll()
+//                .antMatchers(HttpMethod.POST, "/parking-spot").hasRole("USER")
+//                .antMatchers(HttpMethod.DELETE, "/parking-spot/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
