280843b0bacb4aecd9d6a1c0dd59127841412a4c
Daiana Michels <daianaem.12@gmail.com>
2022-08-27 13:43:16 -0300

    feat: finaliza configuração inicial do spring security

--- src/main/java/com/api/parkingcontrol/configurations/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingcontrol/configurations/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.api.parkingcontrol.configurations.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,6 +11,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    final
+    UserDetailsServiceImpl userDetailsService;
+
+    public WebSecurityConfig(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
@@ -25,10 +33,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     //método para substituir as configurações padrão do usuário do spring security:
     @Override
     protected  void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("Admin")
-                .password(passwordEncoder().encode("270822"))
-                .roles("ADMIN");
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
     //configuração do password encoder
